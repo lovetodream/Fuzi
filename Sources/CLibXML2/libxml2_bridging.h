cb61417467d88a6c0b839b8c3c8db58f7b8acb5f
Timo <38291523+lovetodream@users.noreply.github.com>
2026-01-20 14:27:54 +0100

    headers

--- Sources/CLibXML2/libxml2_bridging.h
+++ Sources/CLibXML2/libxml2_bridging.h
@@ -1,9 +1,6 @@
 #ifndef libxml2_import_h
 #define libxml2_import_h
 
-#import <libxml2/libxml/xmlreader.h>
-#import <libxml2/libxml/parser.h>
-#import <libxml2/libxml/xpath.h>
-#import <libxml2/libxml/xpathInternals.h>
+#import <libxml2/libxml/*.h>
 
 #endif /* libxml2_import_h */
