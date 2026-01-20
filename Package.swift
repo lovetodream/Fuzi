8a3afbee151ede3ce5937b5b8463142a1055b0c5
Timo <38291523+lovetodream@users.noreply.github.com>
2026-01-20 14:26:12 +0100

    fix deps

--- Package.swift
+++ Package.swift
@@ -1,25 +1,25 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.10
 import PackageDescription
+
+#if os(Linux)
+let dependencies: [Target.Dependency] = [.target(name: "CLibXML2")]
+let clibs: [Target] = [.systemLibrary(
+    name: "CLibXML2",
+    pkgConfig: "libxml-2.0",
+    providers: [.brew(["libxml2"]), .apt(["libxml2-dev"])]
+)]
+#else
+let dependencies: [Target.Dependency] = []
+let clibs: [Target] = []
+#endif
 
 let package = Package(
     name: "Fuzi",
     products: [
         .library(name: "Fuzi", targets: ["Fuzi"]),
     ],
-    targets: [
-        .target(name: "Fuzi",
-            path: "Sources",
-        ),
-        .systemLibrary(name: "CLibXML2", pkgConfig: "libxml-2.0",
-                       providers: [
-                        .brew(["libxml2"]),
-                        .apt(["libxml2-dev"])
-                       ]),
-        .testTarget(name: "FuziTests",
-                    dependencies: ["Fuzi"],
-                    path: "Tests"
-        )
+    targets: clibs + [
+        .target(name: "Fuzi", dependencies: dependencies),
+        .testTarget(name: "FuziTests", dependencies: ["Fuzi"], path: "Tests")
     ]
 )
